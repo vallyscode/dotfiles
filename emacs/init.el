cde9fa579ffc226620edea9e3c624925719b0b53
Valerii Lysenko <funpixcode@gmail.com>
2018-06-26 22:56:11 +0200

    Changed minor modes symbols

--- emacs/init.el
+++ emacs/init.el
@@ -8,7 +8,8 @@
                     ("melpa-stable" . "https://stable.melpa.org/packages/"))
  package-archive-priorities '(("melpa-stable" . 1)))
 
-(package-initialize)
+(unless package--initialized
+  (package-initialize t))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (when (not package-archive-contents)
@@ -20,17 +21,4 @@
 
 (when (file-readable-p "~/.emacs.d/config.org")
   (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (doneburn-theme yasnippet yaml-mode which-key web-mode use-package tide rainbow-mode rainbow-delimiters popup-kill-ring org-bullets neotree markdown-mode magit less-css-mode jsx-mode json-mode js2-mode intero hindent highlight-symbol helm-projectile groovy-mode evil-nerd-commenter evil-leader delight))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
