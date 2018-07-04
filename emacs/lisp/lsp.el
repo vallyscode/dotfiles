17556ef724249d69a8c8dd3e5156c5436fe13cc2
Valerii Lysenko <funpixcode@gmail.com>
2018-07-04 17:42:27 +0200

    Saving work

--- emacs/lisp/lsp.el
+++ emacs/lisp/lsp.el
@@ -1,78 +1,120 @@
-;;; package -- Summary
+;;; lsp.el --- LSP client -*- coding: utf-8; lexical-binding: t; -*-
+
+;; Copyright (C) 2018 Valerii Lysenko
+;; License: http://www.gnu.org/licenses/gpl.html
+
+;; Author: Valerii Lysenko
+
 ;;; Commentary:
+
+;; yet another one LSP client implementation for Emacs.
+
 ;;; Code:
 
 (require 'json)
 
-;; (defun start-server()
-;;   (interactive)
-;;   (start-process "my-process" "foo" "ls" "-la" "/bin"))
+;;
+;; Constants
+;;
 
-;; (defconst start-command '("ls" "-la" "/bin")
-;;   "A command to start server instance.")
+(defconst boson-log-buffer-name "*lsp*"
+  "Logging buffer name.")
 
-(defconst jdt-lsp-url "http://download.eclipse.org/jdtls/snapshots/jdt-language-server-latest.tar.gz"
-  "Eclipse JDT LSP server.")
+;;
+;; Customization
+;;
 
-(defconst jdt-lsp-file "jdt-language-server-latest.tar.gz"
-  "JDT file name.")
+(defvar lsp-lsp-jdt-url "http://download.eclipse.org/jdtls/snapshots/jdt-language-server-latest.tar.gz"
+  "URL for JDT server download.")
 
-(defconst lsp-download-dir (locate-user-emacs-file "lsp-download/")
-  "Download directory.")
+(defvar lsp-lsp-jdt-dir "lsp-jdt-dir/"
+  "JDT server location.")
 
-(defun lsp-download()
-  "Download files."
-  (interactive)
-  (let ((destination lsp-download-dir))
-    (unless (file-exists-p destination)
-      (make-directory destination t))
-    (message (format "Downloading module from: %s to: %s. Please wait..."
-                     jdt-lsp-url
-                     destination))
-    (url-handler-mode t)
-    (if (file-exists-p jdt-lsp-url)
-        (progn
-          (url-copy-file jdt-lsp-url (format "%s%s" destination jdt-lsp-file ))
-          (message (format "Download complete from: %s to: %s"
-                           jdt-lsp-url
-                           destination)))
-      (error "Not found: %s" jdt-lsp-url))
-    )
-  (lsp-log "Downloading...."))
+(defvar lsp-lsp-jdt-file "jdt-language-server-latest.tar.gz"
+  "JDT server file name.")
 
-(defvar server-process nil
-  "Contains an object describing server process.")
-
-(defconst lsp-start-command '("echo-hs-exe")
+(defvar lsp-lsp-server-command '("ls" "-la" "/bin")
   "A command to start a server.")
 
+(defvar lsp-process nil
+  "Contains an object describing server process.")
+
+(defvar lsp-process-name "lsp-server"
+  "Process name.")
+
+;;
+;; Public
+;;
+
 (defun lsp-log(message)
-  "Log to dedicated notifications buffer."
-  (let* ((log-buffer (get-buffer-create "*lsp*")))
+  "Logging MESSAGE to dedicated log buffer *lsp*."
+  (let ((log-buffer (get-buffer-create boson-log-buffer-name)))
     (with-current-buffer log-buffer
       (insert (format "%s" message)))))
 
-(defun lsp--filter(process output)
-  "LSP process filter."
-  (lsp-log (format "LSP: %s" output)))
+(defun lsp-download(url directory file)
+  "Download FILE to DIRECTORY from URL."
+  (unless (file-exists-p directory)
+    (make-directory directory t))
+  (message
+   (format "Downloading from: %s to: %s. Please wait..."
+           url
+           directory))
+  (url-handler-mode t)
+  (if (file-exists-p url)
+      (progn
+        (url-copy-file url (concat directory file))
+        (message (format "Download complete from: %s to %s"
+                         url
+                         directory)))
+    (error "Not found: %s" url)))
 
-(defun lsp--sentinel(process event)
-  "LSP process sentinel."
-  (lsp-log event))
+(defun lsp-download-test()
+  "Test function."
+  (interactive)
+  (lsp-download lsp-lsp-jdt-url (locate-user-emacs-file lsp-lsp-jdt-dir) lsp-lsp-jdt-file))
 
-(defun lsp-start-server()
+(defun boson-start-server()
   "Start LSP server."
   (interactive)
-  (setq server-process
-        (make-process
-         :name "my-process"
-         :connection-type 'pipe
-         :coding 'no-conversion
-         :command lsp-start-command
-         :filter 'lsp--filter
-         :sentinel 'lsp--sentinel
-         ;; :buffer "lsp-server-buffer"
-         :noquery t)))
+  (setq
+   lsp-process (make-process
+                         :name lsp-process-name
+                         :connection-type 'pipe
+                         :coding 'no-conversion
+                         :command lsp-lsp-server-command
+                         :filter 'lsp--process-filter
+                         :sentinel 'lsp--process-sentinel
+                         ;; :buffer "lsp-server-buffer"
+                         :noquery t)))
+
+(defun lsp-stop-server()
+  "Stops server process."
+  (interactive)
+  (kill-process lsp-process))
+
+(defun lsp-send-wait(message)
+  "Send MESSAGE to server process and simulate waiting."
+  (message (format "Send: %s %s" message lsp-process))
+  (process-send-string lsp-process message)
+  (with-local-quit
+    (let* ((send-time (time-to-seconds (current-time)))
+           (expected-time (+ send-time 10)))
+      (accept-process-output lsp-process (- expected-time (time-to-seconds (current-time))))
+      (when (< expected-time (time-to-seconds (current-time)))
+        (error "WAIT FAIL")))))
+
+;;
+;; Private
+;;
+
+(defun lsp--process-filter(process output)
+  "Get OUTPUT from PROCESS."
+  (lsp-log (format "FILTER: %s" output)))
+
+(defun lsp--process-sentinel(process event)
+  "Get EVENT related to PROCESS."
+  (lsp-log (format "SENTINEL: %s" event)))
 
 (provide 'lsp)
 
